7dc156c78a104095220db1e3a42a11d0b5b0eef4
Everton santos <everton0102@gmail.com>
2021-05-17 19:50:25 -0300

    Servidor completo GET, POST, PUT. PATCH

--- API-REST/aula01/index.js
+++ API-REST/aula01/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+
+
 
 //GET /instrutores
 //GET / instrutores/5
@@ -29,9 +34,14 @@ const listaDeInstrutores = [
         id:4,
         nome: "Victor Magalhaes",
         idade: 28,
-        areaDeAtuacao:"Fron-End"
+        areaDeAtuacao:"Front-End"
     }
+    
+    
 ];
+
+        let proximoId = 5;
+
 app.get("/instrutores",(req, res)=>{
     res.json(listaDeInstrutores);
 });
@@ -41,5 +51,89 @@ app.get("/instrutores/:idConsultado", (req, res)=>{
     const instrutor = listaDeInstrutores.find(instrutor => instrutor.id === Number(req.params.idConsultado));
     res.json(instrutor);
 })
+
+//POST/instrutores |criar Novos recursos na coleção.|
+
+app.post("/instrutores/", (req, res)=>{
+    console.log(req.body);
+    
+    const novoInstrutor ={
+        id: proximoId,
+        nome: req.body.nome,
+        idade: req.body.idade,
+        areaDeAtuacao: req.body.areaDeAtuacao
+    };
+
+    listaDeInstrutores.push(novoInstrutor);
+
+    proximoId += 1;
+    
+    res.send(novoInstrutor);
+
+});
+
+
+
+//Patch instrutores/3    Editar um elemento que ja ta na coleção.
+
+ app.patch("/instrutores/:idConsultado", (req, res)=>{
+    const instrutor = listaDeInstrutores.find(instrutor => instrutor.id === Number(req.params.idConsultado));
+    
+    if(req.body.nome !== undefined){
+        instrutor.nome =  req.body.nome;
+    }
+
+    if(req.body.idade !== undefined){
+        instrutor.idade = req.body.idade;
+    }
+
+    if(req.body.areaDeAtuacao !== undefined){
+        instrutor.areaDeAtuacao = req.body.areaDeAtuacao;
+    }
+
+    res.json(instrutor);
+
+
+ });
+
+
+ //PUT  substituir um elemento da coleção/ se o id existe substitui, se não cria um novo id
+
+
+  app.put("/instrutores/:idConsultado", (req, res) =>{
+    const instrutor = listaDeInstrutores.find(
+        (instrutor) => instrutor.id === Number(req.params.idConsultado)
+    );
+    if(instrutor){
+        //substituir o existente
+        instrutor.nome = req.body.nome;
+        instrutor.idade = req.body.idade;
+        instrutor.areaDeAtuacao = req.body.areaDeAtuacao;
+        res.json(instrutor);
+    }else{
+        //inserir
+        const novoInstrutor = req.body;
+        listaDeInstrutores.push(novoInstrutor);
+        res.json(novoInstrutor);
+    }
+
+  });
+
+  // DELETE
+
+   app.delete("/instrutores/:idConsultado", (req, res) =>{
+    const instrutor = listaDeInstrutores.find(
+        (instrutor) => instrutor.id === Number(req.params.idConsultado)
+    );
+
+    const  indice = listaDeInstrutores.indexOf(instrutor);
+
+    listaDeInstrutores.splice(indice, 1);
+
+    res.json(instrutor);
+
+   });
+
+
 
 app.listen(8000);
